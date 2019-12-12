1778f2848c270a60262a0ff542848615c9d51339
gyulpwang <rbfla927@gmail.com>
2019-12-13 00:06:41 +0900

    페이지별 html 적용

--- profile/src/containers/Header.js
+++ profile/src/containers/Header.js
@@ -1,27 +1,26 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router';
-//import Nav from './Nav';
+
+import profile from '../components/img/profile.jpg';
 
 const MenuItem = ({active, children, to}) => (
-    <li className="nav-item">
-        {/* <a className="nav-link js-scroll-trigger" href={to}>
-            {children} 
-        </a> */}
-        <Link to={to} className="nav-link js-scroll-trigger">
-            {children}
-        </Link>
-    </li>
+    <Link to={to} className="nav-link js-scroll-trigger">
+        {children}
+    </Link>
 );
 
 const Header = (props, context) => {
     const { router } = context;
+    
     return (
         <Fragment>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
                     <span className="d-block d-lg-none">GYULPWANG!</span>
                     <span className="d-none d-lg-block">
-                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="" />
+                        <MenuItem to={'/'} active={router.isActive('/')}>
+                            <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="profile" />
+                        </MenuItem>
                     </span>
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,12 +28,24 @@ const Header = (props, context) => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav">
-                        <MenuItem to={'/about'} active={router.isActive('/about')}>About</MenuItem>
-                        <MenuItem to={'/experience'} active={router.isActive('/experience')}>Experience</MenuItem>
-                        <MenuItem to={'/education'} active={router.isActive('/education')}>Education</MenuItem>
-                        <MenuItem to={'/skills'} active={router.isActive('/skills')}>Skills</MenuItem>
-                        <MenuItem to={'/interests'} active={router.isActive('/interests')}>Interests</MenuItem>
-                        <MenuItem to={'/awards'} active={router.isActive('/awards')}>Awards</MenuItem>
+                        <li className="nav-item">
+                            <MenuItem to={'/about'} active={router.isActive('/about')}>About</MenuItem>
+                        </li>
+                        <li className="nav-item">
+                            <MenuItem to={'/experience'} active={router.isActive('/experience')}>Experience</MenuItem>
+                        </li>
+                        <li className="nav-item">
+                            <MenuItem to={'/education'} active={router.isActive('/education')}>Education</MenuItem>
+                        </li>
+                        <li className="nav-item">
+                            <MenuItem to={'/skills'} active={router.isActive('/skills')}>Skills</MenuItem>
+                        </li>
+                        <li className="nav-item">
+                            <MenuItem to={'/interests'} active={router.isActive('/interests')}>Interests</MenuItem>
+                        </li>
+                        <li className="nav-item">
+                            <MenuItem to={'/awards'} active={router.isActive('/awards')}>Awards</MenuItem>
+                        </li>
                     </ul>
                 </div>
             </nav>
---
+++ profile/src/containers/index.js
@@ -0,0 +1,13 @@
+/*
+    src/containers/index.js
+    앞으로 불러올 컴포넌트의 경로를 편하게 별칭을 부여하기 위해서 만들어진 환경설정 파일
+    형식) export {default as 별칭명} from '경로 포함해서 불러올 파일명'
+*/
+export {default as App} from './App';
+export {default as Home} from './Home';
+export {default as About} from './About';
+export {default as Awards} from './Awards';
+export {default as Education} from './Education';
+export {default as Experience} from './Experience';
+export {default as Interests} from './Interests';
+export {default as Skills} from './Skills';
