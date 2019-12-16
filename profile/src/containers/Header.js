c0419100bb56b7f805a975fe587cb03eb4bf8a37
gyulpwang <rbfla927@gmail.com>
2019-12-16 20:52:49 +0900

    Header 페이지의 MenuItem 통합 및 a tag 오류 해결

--- profile/src/containers/Header.js
+++ profile/src/containers/Header.js
@@ -6,42 +6,24 @@ import ExperienceLinks from '../containers/experience/ExperienceLinks'
 import profile from '../components/img/profile.jpg';
 import '../components/css/custom.css';
 
-const MenuHome = ({active, children, to, id}) => (
-    <Link id={id} to={to}>
-        {children}
-    </Link>
-);
 
 const MenuItem = ({active, children, to, id}) => (
-    //  className='nav-link js-scroll-trigger'
-    <Link id={id} to={to}>
+    <Link id={id} to={to} className={(id === 'home') ? 'navbar-brand js-scroll-trigger' : 'nav-link js-scroll-trigger'}>
         {children}
     </Link>
 );
-
-// TODO 화면이 다 그려지고 난 후 Link에 클래스를 줘볼까?
-/* componentDidUpdate(prevProps, prevState){
-    if (prevState !== this.state) {
-        document.getElementsByClassName('nav-item').getElementsByTagName("a").add('className', 'nav-link js-scroll-trigger');
-    }
-};*/
 
 const Header = (props, context) => {
     const { router } = context;
-
     return (
         <Fragment>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-                <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                    <MenuHome to={'/'} active={router.isActive('/')}>
-                        <span className="d-block d-lg-none">GYULPWANG!</span>
-                    </MenuHome>
+                <MenuItem to={'/'} active={router.isActive('/')} id={'home'}>
+                    <span className="d-block d-lg-none">GYULPWANG!</span>
                     <span className="d-none d-lg-block">
-                        <MenuHome to={'/'} active={router.isActive('/')}>
-                            <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="profile" />
-                        </MenuHome>
+                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="profile" />
                     </span>
-                </a>
+                </MenuItem>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
